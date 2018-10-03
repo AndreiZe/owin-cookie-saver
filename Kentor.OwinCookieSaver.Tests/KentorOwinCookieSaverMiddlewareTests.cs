904af1539b7f550ea95f66a4acc3611c73780c08
REDMOND\andreize <andreize@microsoft.com>
2018-10-03 15:23:21 -0700

    Remove test for FormatHeader property

--- Kentor.OwinCookieSaver.Tests/KentorOwinCookieSaverMiddlewareTests.cs
+++ Kentor.OwinCookieSaver.Tests/KentorOwinCookieSaverMiddlewareTests.cs
@@ -106,7 +106,6 @@ namespace Kentor.OwinCookieSaver.Tests
             cookie.Expires.Should().Be(expectedExpires);
             cookie.Secure.Should().BeTrue("cookie string contains Secure");
             cookie.HttpOnly.Should().BeTrue("cookie string contains HttpOnly");
-            cookie.IsFromHeader().Should().BeTrue();
         }
 
         [TestMethod]
